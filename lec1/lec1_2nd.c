fd0a51f37d6e35f2bbbfef0b85a6090991e51224
KIM HYUNGGYUN <hyunggyunkim@yonsei.ac.kr>
2026-03-13 05:16:37 +0000

    두 정수의 합과 차를 계산하는 프로그램 추가

--- lec1/lec1_2nd.c
+++ lec1/lec1_2nd.c
@@ -2,6 +2,16 @@
 
 int main(){
 
-    
+    int num1, num2;
+
+    printf("첫 번째 정수 : ");
+    scanf("%d", &num1);
+    printf("두 번째 정수 : ");
+    scanf("%d", &num2);
+
+    printf("입력한 정수는 각각 [%d]와 [%d]입니다.\n", num1, num2);
+    printf("---------\n");
+    printf("두 정수의 합은 [%d]입니다.\n", num1 + num2);
+    printf("두 정수의 차는 [%d]입니다.\n", num1 - num2);
     return 0;
 }
