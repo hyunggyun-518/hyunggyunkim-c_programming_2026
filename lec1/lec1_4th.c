f2f86f2e32bbaae539d13d0ffb43978cbf56bd55
KIM HYUNGGYUN <hyunggyunkim@yonsei.ac.kr>
2026-03-13 05:38:24 +0000

    lec1_3rd.c 및 lec1_4th.c 파일 추가: 정수, 실수, 문자 입력 및 나머지 연산 출력 프로그램 구현

---
+++ lec1/lec1_4th.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 2;
+    int a2 = 2;
+    int b = 3;
+    int c = 3;
+    printf("a % b; %d\n", a % b);
+    printf("a %%% c: %f\n", a % c);
+    printf("a2 %%% b: %f\n", a2 % b);
+    return 0;
+}
